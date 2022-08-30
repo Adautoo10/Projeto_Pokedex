9395ecc8ff304df3bba0d1c6bf3a5ba81f65c8af
Adautoo10 <adauto.facin05@gmail.com>
2022-08-30 15:37:57 -0300

    Adicionando Cards na Home

--- View/modules/pagina_inicio/Home.php
+++ View/modules/pagina_inicio/Home.php
@@ -29,7 +29,46 @@
 </ul>
 
 
+
 </center>
+
+
+<br>
+
+
+<div class="card" style="width: 18rem;">
+  <img src="https://sm.ign.com/ign_br/screenshot/default/025pikachu-os-anime-4_3pym.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Pikachu</h5>
+    <p class="card-text">Pikachu é um tipo de Pokémon elétrico: os bolsos em suas bochechas, identificado por discos vermelhos que decoram, são capazes de gerar choques elétricos de intensidade variável, chegando até o nível de poder de um raio. Eles também o usam para se defender.</p>
+  </div>
+  <ul class="list-group list-group-flush"> 
+    <li class="list-group-item">=====HABILIDADES=====</li>
+    <li class="list-group-item">Ataque Rápido</li>
+    <li class="list-group-item">Bola de Fogo</li>
+
+  </ul>
+
+</div>
+
+  <br> <br>
+  <div class="card" style="width: 18rem;" >
+  <img src="https://mestrepokemon.com/wp-content/uploads/2020/03/Pok%C3%A9mon-Pidgey-Pok%C3%A9dex.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Pidgey</h5>
+    <p class="card-text">Pidgey ,além de ser um pokemon normal ,tem uma grande força. Pidgey pode voar até mesmo para outras regiões no inverno ,além de usar gust para assustar pokemons no frio ,podendo se alimentar de frutas de seus caçadores.
+
+</p>
+  </div>
+  <ul class="list-group list-group-flush"> 
+    <li class="list-group-item">=====HABILIDADES=====</li>
+    <li class="list-group-item">Ataque Rápido</li>
+    <li class="list-group-item">Ataque de Vento</li>
+
+  </ul>
+
+
+</div>
 
 
 </body>
